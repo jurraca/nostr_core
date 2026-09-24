c9ce06f1b12a80b175cdc282c15b83e35ca3a58c
jurraca <julienu@pm.me>
2026-09-24 17:15:04 +0200

    deps: update deps, including libsecp256k1; add deps_nix, regenerate deps.nix

--- nix/deps.nix
+++ nix/deps.nix
@@ -1,8 +1,18 @@
 {
-  pkgs,
   lib,
   beamPackages,
+  cmake,
+  extend,
+  lexbor,
+  fetchFromGitHub,
+  oniguruma,
   overrides ? (x: y: { }),
+  overrideFenixOverlay ? null,
+  rustlerPrecompiledOverrides ? { },
+  stdenv,
+  pkg-config,
+  vips,
+  writeText,
 }:
 
 let
@@ -11,23 +21,30 @@ let
 
   workarounds = {
     portCompiler = _unusedArgs: old: {
-      buildPlugins = [ pkgs.beamPackages.pc ];
+      buildPlugins = [ beamPackages.pc ];
     };
 
     rustlerPrecompiled =
       {
         toolchain ? null,
+        buildInputs ? [ ],
+        nativeBuildInputs ? [ ],
+        env ? { },
         ...
       }:
       old:
       let
-        extendedPkgs = pkgs.extend fenixOverlay;
-        fenixOverlay = import "${
-          fetchTarball {
-            url = "https://github.com/nix-community/fenix/archive/056c9393c821a4df356df6ce7f14c722dc8717ec.tar.gz";
-            sha256 = "sha256:1cdfh6nj81gjmn689snigidyq7w98gd8hkl5rvhly6xj7vyppmnd";
-          }
-        }/overlay.nix";
+        extendedPkgs = extend fenixOverlay;
+        fenixOverlay =
+          if overrideFenixOverlay == null then
+            import "${
+              fetchTarball {
+                url = "https://github.com/nix-community/fenix/archive/6399553b7a300c77e7f07342904eb696a5b6bf9d.tar.gz";
+                sha256 = "sha256-C6tT7K1Lx6VsYw1BY5S3OavtapUvEnDQtmQB5DSgbCc=";
+              }
+            }/overlay.nix"
+          else
+            overrideFenixOverlay;
         nativeDir = "${old.src}/native/${with builtins; head (attrNames (readDir "${old.src}/native"))}";
         fenix =
           if toolchain == null then
@@ -35,21 +52,23 @@ let
           else
             extendedPkgs.fenix.fromToolchainName toolchain;
         native =
-          (extendedPkgs.makeRustPlatform {
-            inherit (fenix) cargo rustc;
-          }).buildRustPackage
+          (
+            (extendedPkgs.makeRustPlatform {
+              inherit (fenix) cargo rustc;
+            }).buildRustPackage
             {
-              pname = "${old.packageName}-native";
+              inherit env buildInputs;
+              pname = "${old.beamModuleName}-native";
               version = old.version;
               src = nativeDir;
               cargoLock = {
                 lockFile = "${nativeDir}/Cargo.lock";
               };
-              nativeBuildInputs = [
-                extendedPkgs.cmake
-              ] ++ extendedPkgs.lib.lists.optional extendedPkgs.stdenv.isDarwin extendedPkgs.darwin.IOKit;
+              nativeBuildInputs = [ extendedPkgs.cmake ] ++ nativeBuildInputs;
               doCheck = false;
-            };
+            }
+          ).overrideAttrs
+            rustlerPrecompiledOverrides.${old.beamModuleName} or { };
 
       in
       {
@@ -62,11 +81,17 @@ let
           mkdir -p priv/native
           for lib in ${native}/lib/*
           do
-            ln -s "$lib" "priv/native/$(basename "$lib")"
+            dest="$(basename "$lib")"
+            if [[ "''${dest##*.}" = "dylib" ]]
+            then
+              dest="''${dest%.dylib}.so"
+            fi
+            dest="''${dest#lib}"
+            ln -s "$lib" "priv/native/$dest"
           done
         '';
 
-        buildPhase = ''
+        preBuild = ''
           suggestion() {
             echo "***********************************************"
             echo "                 deps_nix                      "
@@ -83,14 +108,35 @@ let
             echo -n " "
             grep -Rl 'use RustlerPrecompiled' lib \
               | xargs grep 'defmodule' \
-              | sed 's/defmodule \(.*\) do/config :${old.packageName}, \1, skip_compilation?: true/'
+              | sed 's/defmodule \(.*\) do/config :${old.beamModuleName}, \1, skip_compilation?: true/'
             echo "***********************************************"
             exit 1
           }
           trap suggestion ERR
-          ${old.buildPhase}
         '';
       };
+
+    elixirMake = _unusedArgs: old: {
+      preConfigure = ''
+        export ELIXIR_MAKE_CACHE_DIR="$TEMPDIR/elixir_make_cache"
+      '';
+    };
+
+    lazyHtml = _unusedArgs: old: {
+      preConfigure = ''
+        export ELIXIR_MAKE_CACHE_DIR="$TEMPDIR/elixir_make_cache"
+      '';
+
+      postPatch = ''
+        substituteInPlace mix.exs \
+          --replace-fail "Fine.include_dir()" '"${packages.fine}/src/c_include"' \
+          --replace-fail '@lexbor_git_sha "244b84956a6dc7eec293781d051354f351274c46"' '@lexbor_git_sha ""'
+      '';
+
+      preBuild = ''
+        install -Dm644           -t _build/c/third_party/lexbor/$LEXBOR_GIT_SHA/build           ${lexbor}/lib/liblexbor_static.a
+      '';
+    };
   };
 
   defaultOverrides = (
@@ -107,8 +153,8 @@ let
           {
             name = "rustlerPrecompiled";
             toolchain = {
-              name = "nightly-2024-11-01";
-              sha256 = "sha256-wq7bZ1/IlmmLkSa3GUJgK17dTWcKyf5A+ndS9yRwB88=";
+              name = "nightly-2025-06-23";
+              sha256 = "sha256-UAoZcxg3iWtS+2n8TFNfANFt/GmkuOMDf7QAE0fRxeA=";
             };
           }
         ];
@@ -140,18 +186,18 @@ let
     with self;
     {
 
-      castore =
+      bechamel =
         let
-          version = "1.0.12";
+          version = "1.1.0";
           drv = buildMix {
             inherit version;
-            name = "castore";
+            name = "bechamel";
             appConfigPath = ./config;
 
             src = fetchHex {
               inherit version;
-              pkg = "castore";
-              sha256 = "3dca286b2186055ba0c9449b4e95b97bf1b57b47c1f2644555879e659960c224";
+              pkg = "bechamel";
+              sha256 = "42b3a5720ec94aa2ede20930f88d91efbe64179ae9764dbc2f560ddee102cbe5";
             };
           };
         in
@@ -159,7 +205,7 @@ let
 
       elixir_make =
         let
-          version = "0.9.0";
+          version = "0.10.0";
           drv = buildMix {
             inherit version;
             name = "elixir_make";
@@ -168,87 +214,7 @@ let
             src = fetchHex {
               inherit version;
               pkg = "elixir_make";
-              sha256 = "db23d4fd8b757462ad02f8aa73431a426fe6671c80b200d9710caf3d1dd0ffdb";
-            };
-          };
-        in
-        drv;
-
-      ex_bech32 =
-        let
-          version = "0.5.1";
-          drv = buildMix {
-            inherit version;
-            name = "ex_bech32";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "ex_bech32";
-              sha256 = "ccf16f7030dc6d652fbae8de77c31b5781bdda6492f12c32f604ef8e8e944895";
-            };
-
-            beamDeps = [
-              rustler_precompiled
-            ];
-          };
-        in
-        drv.override (workarounds.rustlerPrecompiled { } drv);
-
-      finch =
-        let
-          version = "0.19.0";
-          drv = buildMix {
-            inherit version;
-            name = "finch";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "finch";
-              sha256 = "fc5324ce209125d1e2fa0fcd2634601c52a787aff1cd33ee833664a5af4ea2b6";
-            };
-
-            beamDeps = [
-              mime
-              mint
-              nimble_options
-              nimble_pool
-              telemetry
-            ];
-          };
-        in
-        drv;
-
-      hpax =
-        let
-          version = "1.0.3";
-          drv = buildMix {
-            inherit version;
-            name = "hpax";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "hpax";
-              sha256 = "8eab6e1cfa8d5918c2ce4ba43588e894af35dbd8e91e6e55c817bca5847df34a";
-            };
-          };
-        in
-        drv;
-
-      jason =
-        let
-          version = "1.4.1";
-          drv = buildMix {
-            inherit version;
-            name = "jason";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "jason";
-              sha256 = "fbb01ecdfd565b56261302f7e1fcc27c4fb8f32d56eab74db621fc154604a7a1";
+              sha256 = "dc1f09fb7fa68866b886abd5f0f3c83553b1a19a52359a899e92af1bb3b31982";
             };
           };
         in
@@ -256,7 +222,7 @@ let
 
       lib_secp256k1 =
         let
-          version = "0.6.0";
+          version = "0.8.0";
           drv = buildMix {
             inherit version;
             name = "lib_secp256k1";
@@ -265,7 +231,7 @@ let
             src = fetchHex {
               inherit version;
               pkg = "lib_secp256k1";
-              sha256 = "985332e774e51b368f5737d20edd79db59bde5621a4d90751e9e1ca17be5ef45";
+              sha256 = "13da3da4cdb85eb660cb743ce43f2538743e92312ad4444cb6bdd53f5c31c643";
             };
 
             beamDeps = [
@@ -273,184 +239,7 @@ let
             ];
           };
         in
-        drv;
-
-      mime =
-        let
-          version = "2.0.6";
-          drv = buildMix {
-            inherit version;
-            name = "mime";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "mime";
-              sha256 = "c9945363a6b26d747389aac3643f8e0e09d30499a138ad64fe8fd1d13d9b153e";
-            };
-          };
-        in
-        drv;
-
-      mint =
-        let
-          version = "1.7.1";
-          drv = buildMix {
-            inherit version;
-            name = "mint";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "mint";
-              sha256 = "fceba0a4d0f24301ddee3024ae116df1c3f4bb7a563a731f45fdfeb9d39a231b";
-            };
-
-            beamDeps = [
-              castore
-              hpax
-            ];
-          };
-        in
-        drv;
-
-      mint_web_socket =
-        let
-          version = "1.0.4";
-          drv = buildMix {
-            inherit version;
-            name = "mint_web_socket";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "mint_web_socket";
-              sha256 = "027d4c5529c45a4ba0ce27a01c0f35f284a5468519c045ca15f43decb360a991";
-            };
-
-            beamDeps = [
-              mint
-            ];
-          };
-        in
-        drv;
-
-      nimble_options =
-        let
-          version = "1.1.1";
-          drv = buildMix {
-            inherit version;
-            name = "nimble_options";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "nimble_options";
-              sha256 = "821b2470ca9442c4b6984882fe9bb0389371b8ddec4d45a9504f00a66f650b44";
-            };
-          };
-        in
-        drv;
-
-      nimble_pool =
-        let
-          version = "1.1.0";
-          drv = buildMix {
-            inherit version;
-            name = "nimble_pool";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "nimble_pool";
-              sha256 = "af2e4e6b34197db81f7aad230c1118eac993acc0dae6bc83bac0126d4ae0813a";
-            };
-          };
-        in
-        drv;
-
-      nostr_lib =
-        let
-          version = "0.1.1";
-          drv = buildMix {
-            inherit version;
-            name = "nostr_lib";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "nostr_lib";
-              sha256 = "5ed8a39f792dae17917b8c119271546a11258e2fac7df5ebf2b5f55626a45c5a";
-            };
-
-            beamDeps = [
-              ex_bech32
-              jason
-              lib_secp256k1
-            ];
-          };
-        in
-        drv;
-
-      req =
-        let
-          version = "0.5.0";
-          drv = buildMix {
-            inherit version;
-            name = "req";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "req";
-              sha256 = "dda04878c1396eebbfdec6db6f3d4ca609e5c8846b7ee88cc56eb9891406f7a3";
-            };
-
-            beamDeps = [
-              finch
-              jason
-              mime
-            ];
-          };
-        in
-        drv;
-
-      rustler_precompiled =
-        let
-          version = "0.8.2";
-          drv = buildMix {
-            inherit version;
-            name = "rustler_precompiled";
-            appConfigPath = ./config;
-
-            src = fetchHex {
-              inherit version;
-              pkg = "rustler_precompiled";
-              sha256 = "63d1bd5f8e23096d1ff851839923162096364bac8656a4a3c00d1fff8e83ee0a";
-            };
-
-            beamDeps = [
-              castore
-            ];
-          };
-        in
-        drv;
-
-      telemetry =
-        let
-          version = "1.2.1";
-          drv = buildRebar3 {
-            inherit version;
-            name = "telemetry";
-
-            src = fetchHex {
-              inherit version;
-              pkg = "telemetry";
-              sha256 = "dad9ce9d8effc621708f99eac538ef1cbe05d6a874dd741de2e689c47feafed5";
-            };
-          };
-        in
-        drv;
+        drv.override (workarounds.elixirMake { } drv);
 
     };
 in
